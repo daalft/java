ef66e0d669a1a2db98d011197c77fbcc322b7d70
daalft <blueygh2@gmail.com>
2014-11-25 12:04:15 +0100

    Create Woche5.java

---
+++ Woche5.java
@@ -0,0 +1,18 @@
+public class Woche5 {
+  public void run () {
+    int i = 0;
+    while (true) {
+      while (true) {
+        if (i >= 100) {
+          break;
+        } else {
+          i++;
+        }
+      }
+    }
+  }
+  
+  public static void main (String[] args) {
+    run();
+  }
+}
